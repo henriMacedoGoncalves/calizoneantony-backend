b94e7660e22a745dec3f319e078f8c4a5ac91a7c
henriMacedoGoncalves <henri.macedogoncalves@gmail.com>
2025-07-21 22:43:21 +0200

    Add restapi for states

--- src/main/java/com/calizoneantony/backend/dao/StateRepository.java
+++ src/main/java/com/calizoneantony/backend/dao/StateRepository.java
@@ -2,6 +2,13 @@ package com.calizoneantony.backend.dao;
 
 import com.calizoneantony.backend.entity.State;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+import java.util.List;
+
+@RepositoryRestResource
 public interface StateRepository extends JpaRepository<State, Long> {
+
+    List<State> findByCountryCode(@Param("code") String code);
 }
